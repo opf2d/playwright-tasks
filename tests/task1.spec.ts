e5262fc37e1c43b49e89dbc5b9d0d945d9e873d0
opf2d <fouad.lathqani@sociumtech.com>
2025-04-10 18:54:25 +0300

    added each : test.describe and test.beforeEach

--- tests/task1.spec.ts
+++ tests/task1.spec.ts
@@ -1,26 +1,27 @@
 import { expect, test, Page } from "@playwright/test";
-// const mainUrl: string = "https://www.saucedemo.com/";
 
 async function login(page: Page, userName: string, passWord: string) {
-  await page.goto('/');
   await page.locator('[id="user-name"]').fill(userName);
   await page.locator('[id="password"]').fill(passWord);
   await page.locator('[id="login-button"]').click();
 }
 
-test("Verify Test successful login on saucedemo", async ({ page }) => {
-  await login(page, "standard_user", "secret_sauce");
-  await expect(page.locator('[id="react-burger-menu-btn"]')).toBeVisible();
-  await page.locator('[id="react-burger-menu-btn"]').click();
-  await page.locator('[id="logout_sidebar_link"]').click();
-});
+test.describe("Login Test Cases", () => {
+  test.beforeEach("Opening ", async ({ page }) => {
+    await page.goto("/");
+  });
+  test("Verify logging in sucessfully", async ({ page }) => {
+    await login(page, "standard_user", "secret_sauce");
+    await expect(page.locator("[id='react-burger-menu-btn']")).toBeVisible();
+  });
 
-test("Verify Test login with invalid credentials on saucedemo", async ({ page }) => {
-  await login(page, "invalid_user", "invalidPassword");
-  await expect(page.locator('[data-test="error"]')).toHaveText(/.*do not match any/);
-});
+  test("Verify invalid login", async ({ page }) => {
+    await login(page, "invalid_user", "invalidPassword");
+    await expect(page.locator("[data-test='error']")).toHaveText(/.*do not match any/);
+  });
 
-test("Verify Test login with a locked account on saucedemo", async ({ page }) => {
-  await login(page, "locked_out_user", "secret_sauce");
-  await expect(page.locator('[data-test="error"]')).toHaveText(/.*locked out./);
+  test("Verify locked account logging in", async ({ page }) => {
+    await login(page, "locked_out_user", "secret_sauce");
+    await expect(page.locator("[data-test='error']")).toHaveText(/.*locked out./);
+  });
 });
