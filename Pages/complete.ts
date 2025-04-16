26b8714c1b5eff4a846e4061243b68c5432179c8
opf2d <fouad.lathqani@sociumtech.com>
2025-04-16 15:12:02 +0300

    Redited complete.ts and applied the base.ts functions

--- Pages/complete.ts
+++ Pages/complete.ts
@@ -1,21 +1,21 @@
-import { Page, Locator, expect } from "@playwright/test";
+import { Page, Locator } from '@playwright/test';
+import { BasePage } from './base';
 
-export class SuccessfulMSG {
-  private readonly page: Page;
+export class SuccessfulMSG extends BasePage {
   private readonly successMSG: Locator;
   private readonly backHomeBTN: Locator;
 
   constructor(page: Page) {
-    this.page = page;
-    this.successMSG = page.locator("#checkout_complete_container");
-    this.backHomeBTN = page.locator("#back-to-products");
+    super(page);
+    this.successMSG = page.locator('#checkout_complete_container');
+    this.backHomeBTN = page.locator('#back-to-products');
   }
 
   async sucessful(): Promise<void> {
-    await expect(this.successMSG).toHaveCount(1);
+    await this.verifyElementPresence(this.successMSG, true);
   }
 
   async goBackHome(): Promise<void> {
-    await this.backHomeBTN.click();
+    await this.navigate(this.backHomeBTN);
   }
 }
