fc2381424662930502ea8a2b64d8ff8c1da12d3a
opf2d <fouad.lathqani@sociumtech.com>
2025-04-16 14:41:48 +0300

    Redited products.ts and applied the base.ts functions

--- Pages/products.ts
+++ Pages/products.ts
@@ -1,27 +1,26 @@
-import { Page, Locator, expect } from "@playwright/test";
+import { Page, Locator } from '@playwright/test';
+import { BasePage } from './base';
 
-export class ProductSelection {
-  private readonly page: Page;
+export class ProductSelection extends BasePage {
   private readonly addToCartButtons: Locator;
   private readonly firstItemBTN: Locator;
   private readonly lastItemBTN: Locator;
   private readonly basket: Locator;
 
   constructor(page: Page) {
-    this.page = page;
-    this.addToCartButtons = page.locator('[id^="add-to-cart-"]'); 
+    super(page);
+    this.addToCartButtons = page.locator('[id^="add-to-cart-"]');
     this.firstItemBTN = this.addToCartButtons.nth(0);
     this.lastItemBTN = this.addToCartButtons.last();
-    this.basket = page.locator(".shopping_cart_link");
+    this.basket = page.locator('.shopping_cart_link');
   }
 
   async addItem(): Promise<void> {
-    await this.firstItemBTN.click();
-    await this.lastItemBTN.click();
+    await this.clickElement(this.firstItemBTN);
+    await this.clickElement(this.lastItemBTN);
   }
 
   async goToCart(): Promise<void> {
-    await this.basket.click();
+    await this.navigate(this.basket);
   }
 }
-
